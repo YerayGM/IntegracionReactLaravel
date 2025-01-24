45da761f6ef6a4c540c0944921c472042a15b69c
YerayGM <yeraygarciamartinez5@gmail.com>
2025-01-24 19:35:09 +0000

    Terminado

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+build: {
+minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+cssMinify: process.env.APP_ENV === 'production',
+},
+plugins: [
+laravel({
+input: ['resources/js/app.jsx'],
+refresh: true,
+}),
+react(),
+],
 });
